ceeb3ee4158b0927b6f4701af2fe1db6a511ec87
Ahnjihyeon <ajh2015@hanmail.net>
2020-02-11 03:43:09 +0900

    spring boot 프로젝트 생성

---
+++ Teatime-back/src/main/java/com/millionware/teatime/TeatimeBackApplication.java
@@ -0,0 +1,13 @@
+package com.millionware.teatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatimeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeatimeBackApplication.class, args);
+	}
+
+}
---
+++ Teatime-back/src/test/java/com/millionware/teatime/TeatimeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millionware.teatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatimeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
